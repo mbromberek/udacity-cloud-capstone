9e20d538595c939b3f47d45d959d9625e1d2d129
mbromberek <mikebromberek@gmail.com>
2019-11-06 19:49:40 -0600

    convert uploaded image to thumbnail

--- backend/src/lambda/s3/editImage.ts
+++ backend/src/lambda/s3/editImage.ts
@@ -33,30 +33,12 @@ async function processImage(record: S3EventRecord) {
       Key: key
     })
     .promise()
-    
-//   let textData = {
-//     text: 'DISTANCE', //the text to be rendered on the image
-//     maxWidth: 1004, //image width - 10px margin left - 10px margin right
-//     maxHeight: 72+20, //logo height + margin
-//     placementX: 10, // 10px in on the x axis
-//     placementY: 1024-(72+20)-10 //bottom of the image: height - maxHeight - margin 
-//   };
-
-//   const font = Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
-  
+      
   const image = await Jimp.read(response.Body)
-//   const body: Buffer = Buffer.from(response.Body, 'utf8')
-//   const body: Buffer = response.Body
-//   const image = await Jimp.read(body)
-// 
+
   logger.info('Resizing image')
   image.resize(300, Jimp.AUTO)
-//   image.print(
-//     font,
-//     textData.placementX,
-//     textData.placementY,
-//     textData.text
-//   )
+
   const convertedBuffer = await image.getBufferAsync(Jimp.AUTO)
 
   logger.info(`Writing image back to S3 bucket: ${updateImgBucketName}`)
@@ -68,3 +50,47 @@ async function processImage(record: S3EventRecord) {
     })
     .promise()
 }
+
+/*
+async function addTextToImage(record: S3EventRecord) {
+  const key = record.s3.object.key
+  logger.info('Processing S3 item with key: ', {'key': key})
+  const response = await s3
+    .getObject({
+      Bucket: imagesBucketName,
+      Key: key
+    })
+    .promise()
+    
+  let textData = {
+    text: 'DISTANCE', //the text to be rendered on the image
+    maxWidth: 1004, //image width - 10px margin left - 10px margin right
+    maxHeight: 72+20, //logo height + margin
+    placementX: 10, // 10px in on the x axis
+    placementY: 1024-(72+20)-10 //bottom of the image: height - maxHeight - margin 
+  };
+
+  const font = Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+  
+  const image = await Jimp.read(response.Body)
+
+  logger.info('Resizing image')
+
+  image.print(
+    font,
+    textData.placementX,
+    textData.placementY,
+    textData.text
+  )
+  const convertedBuffer = await image.getBufferAsync(Jimp.AUTO)
+
+  logger.info(`Writing image back to S3 bucket: ${updateImgBucketName}`)
+  await s3
+    .putObject({
+      Bucket: updateImgBucketName,
+      Key: `${key}.jpeg`,
+      Body: convertedBuffer
+    })
+    .promise()
+}
+*/
